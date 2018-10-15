969ee708390d62934b2ab4e35091127022d34216
Amelia Kim <ameliamjkim@gmail.com>
2018-10-15 17:26:40 -0400

    add-devise-user-authentication

--- Gemfile
+++ Gemfile
@@ -33,7 +33,8 @@ gem 'jbuilder', '~> 2.5'
 gem 'bootsnap', '>= 1.1.0', require: false
 
 group :development, :test do
-
+  gem "rspec-rails"
+  gem "factory_bot_rails"
 end
 
 group :development do
---
+++ spec/factories/user.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :user do
+    first_name "Amelia"
+    last_name "Kim"
+    email "ameliamjkim@gmail.com"
+    password "thisismypassword.123"
+    city "Boston"
+    state "MA"
+  end
+end
